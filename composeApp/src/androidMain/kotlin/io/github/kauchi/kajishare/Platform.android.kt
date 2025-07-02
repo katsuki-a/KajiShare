c27491830139e1be146de490f50b2826a0bdb704
katsuki-a <katsuki1229s@gmail.com>
2025-07-02 23:35:44 +0900

    feat: Display platform name on screen

--- composeApp/src/androidMain/kotlin/io/github/kauchi/kajishare/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/kauchi/kajishare/Platform.android.kt
@@ -1,9 +1,7 @@
 package io.github.kauchi.kajishare
 
-import android.os.Build
-
 class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
+    override val name: String = "Android"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/commonMain/kotlin/io/github/kauchi/kajishare/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/kauchi/kajishare/App.kt
@@ -1,6 +1,11 @@
 package io.github.kauchi.kajishare
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import io.github.kauchi.kajishare.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -8,5 +13,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     AppTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Hello ${getPlatform().name}!")
+        }
     }
 }
